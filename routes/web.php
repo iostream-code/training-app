6637c17959ea7f54db33ed14cd06dc2c7f5efdf4
iostream-code <aryakmifjannah2603@gmail.com>
2023-03-20 18:44:46 +0700

    Reset

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/contact', function () {
-//     return view('contact');
-// });
-
-Route::view('/contact', 'contact', ['name' => 'Iostream', 'job' => 'College Student']);
