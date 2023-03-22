e814866672be7e3d6cb851ecdc264ce5ba76936f
iostream-code <aryakmifjannah2603@gmail.com>
2023-03-22 10:40:34 +0700

    Layout Tmplating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,24 +6,6 @@
     <h1>Ini halaman Home</h1>
     <p>Selamat Datang, {{ $name }}</p>
 
-    {{-- @if ($role == 'Admin')
-            <a href="">Ke Halaman Admin</a>
-        @elseif ($role == 'Staf')
-            <a href="">Ke Halaman Gudang</a>
-        @endif --}}
-
-    {{-- @switch($role)
-            @case('Admin')
-                <a href="">Ke Halaman Admin</a>
-            @break
-
-            @case('Staf')
-                <a href="">Ke Halaman Gudang</a>
-            @break
-
-            @default
-        @endswitch --}}
-
     <table class="table">
         <tr>
             <th>No</th>
